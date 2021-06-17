514987060ed82974f5bb94a710e89e7ad1d3c6a4
walknight <walknight>
2021-06-17 01:56:35 -0500

    add recipe: Air Rescue

---
+++ recipes/AddOn_Air_Rescue/exec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp ./boxart/addon.z.png /tmp
+echo -e "[Property]\nBezelPath=/tmp/addon.z.png" > /tmp/gameinfo.ini
+
+set -x
+/emulator/retroplayer ./emu/mame2003_plus_libretro_20210521.4.so "./roms/arescue.zip"
+
+rm -f /tmp/gameinfo.ini
+
