170f1b054cce262c40ca95ec92888bb18c95b3b3
walknight <walknight>
2021-08-23 00:50:31 -0500

    Recipe: Add Skydiver with MAME artwork

---
+++ recipes/AddOn_Skydiver/exec.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#cp ./boxart/addon.z.png /tmp
+#echo -e "[Property]\nBezelPath=/tmp/addon.z.png" > /tmp/gameinfo.ini
+
+set -x
+
+/emulator/retroplayer ./emu/mame2003_plus_libretro_20210521.4.so "./roms/skydiver.zip"
+
+rm -f /tmp/gameinfo.ini
+
